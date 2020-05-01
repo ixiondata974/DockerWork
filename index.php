3c0c0cdfddc548155bdef526a506b6ae3131b073
thierry <thierry.changf@gmail.com>
2020-05-01 16:22:17 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$mysql = new mysqli("localhost", "root");
+echo "MySQL Server info: ".$mysql->host_info;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Voila !!!</h1>
+</body>
+</html>
