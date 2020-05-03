4f6cb947f207a07498555e2488d90b2e34658e84
thierry <thierry.changf@gmail.com>
2020-05-03 18:21:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 $mysql = new mysqli("localhost", "root");
 echo "MySQL Server info: ".$mysql->host_info;
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Voila </h1>
+</body>
+</html>
